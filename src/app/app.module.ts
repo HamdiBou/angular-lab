e9d460802d8ff66f233044ff3112e04b915e948d
hamdi <h.boussarsar11@gmail.com>
2025-02-11 13:08:06 +0100

    `Added form functionality to member form component`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,11 @@ import { MemberComponent } from './member/member.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
 import { MemberFormComponent } from './member-form/member-form.component';
-
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,8 +25,11 @@ import { MemberFormComponent } from './member-form/member-form.component';
     BrowserAnimationsModule,
     MatTableModule,
     HttpClientModule,
-    MatIconModule
-
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/member-form/member-form.component.ts
+++ src/app/member-form/member-form.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-member-form',
   templateUrl: './member-form.component.html',
   styleUrls: ['./member-form.component.css']
 })
-export class MemberFormComponent {
+export class MemberFormComponent implements OnInit
+{
+form !: FormGroup;
 
+ngOnInit(): void {
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+  this.form = new FormGroup({
+    cin : new FormControl(null, Validators.required),
+    name : new FormControl(null, Validators.required),
+    type : new FormControl(null, Validators.required),
+    createdDate: new FormControl(null, Validators.required),
+  });
+}
 }
